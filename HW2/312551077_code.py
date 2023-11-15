b4f14d9feb3086454342a4bb2ed30c0509373419
michael21910 <faefae31910@gmail.com>
2023-11-15 19:04:21 +0800

    [ 2023/11/15 ] [ Update HW2, unfinished ]

--- HW2/312551077_code.py
+++ HW2/312551077_code.py
@@ -3,19 +3,19 @@ import matplotlib.pyplot as plt
 import random
 
 # create a ring lattice
-def create_ring_lattice(n, k):
-    ring_lattice = {}
+def createRingLattice(n, k):
+    ringLattice = {}
     for i in range(n):
-        ring_lattice[i] = set()
+        ringLattice[i] = set()
         for j in range(i - k // 2, i):
-            ring_lattice[i].add(j % n)
+            ringLattice[i].add(j % n)
         for j in range(i + 1, i + 1 + k // 2):
-            ring_lattice[i].add(j % n)
-    return ring_lattice
+            ringLattice[i].add(j % n)
+    return ringLattice
 
 # create a WS model
-def create_WS_model(n, k, p):
-    ring_lattice = create_ring_lattice(n, k)
+def createWSModel(n, k, p):
+    ring_lattice = createRingLattice(n, k)
     for i in range(n):
         for j in range(i + 1, i + 1 + k // 2):
             if random.random() < p:
@@ -23,5 +23,14 @@ def create_WS_model(n, k, p):
                 ring_lattice[i].add(random.randint(0, n - 1))
     return ring_lattice
 
-WSModel = create_WS_model(20, 4, 0.1)
-print(WSModel)
+# 1-1: create model of [ Maximal Independent Set (MIS) Game (Symmetric) ]
+def createMISModel(n, k, p):
+    pass
+
+# 1-2: create model of [ Asymmetric MDS-based IDS Game ]
+def createAMDSIDSModel(n, k, p):
+    pass
+
+# 2: Maximum Matching Problem
+def maximumMatchingProblem(n, k, p):
+    pass
